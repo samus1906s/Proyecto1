99e3096860bced311e43738a05a11b6dd272fbf7
samus <samuelcedas19@gmail.com>
2025-07-27 20:21:02 -0600

    Sistema Iluminación
    Se crea esta subclase en la cual cumplirá función del sistema de iluminación para el vehículo con una java class y un java enum

---
+++ src/SistemaIluminacion/TipoLuz.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package SistemaIluminacion;
+import java.awt.Color;
+/**
+ *
+ * @author samue
+ */
+
+public enum TipoLuz {
+    DELANTERA_BAJA(Color.WHITE),
+    DELANTERA_ALTA(Color.WHITE),
+    INTERMITENTE(Color.ORANGE),
+    EMERGENCIA(Color.RED),
+    PUERTAS(Color.BLUE);
+
+    private final Color color;
+    
+     public Color getColor() {
+        return color;
+    }
+
+    TipoLuz(Color color) {
+        this.color = color;
+    }
+
+   
+}
+
