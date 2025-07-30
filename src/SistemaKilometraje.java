96560738d2bd0e4a75f335b8974571368c8445ff
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-07-30 12:31:02 -0600

    Sistema Kilometraje
    Se creo la clase Kilometraje con sus respectivos metodos correctamente

---
+++ src/SistemaKilometraje.java
@@ -0,0 +1,61 @@
+
+import AcelerarVehiculo.Acelerar;
+import CombustibleVehiculo.Combustible;
+import MotorVehiculo.Motor;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Eduard Salas Murillo
+ */
+public class SistemaKilometraje {
+    private double kilometrajeTotal;
+    private double kilometrajeViaje; 
+    private int velocidadActual; 
+    private Acelerar rpm; 
+    private Motor motor; 
+    private Combustible combustible; 
+    
+    public double getKilometrajeTotal() {
+        return kilometrajeTotal;
+    }
+
+    public double getKilometrajeViaje() {
+        return kilometrajeViaje;
+    }
+
+    public int getVelocidadActual() {
+        return velocidadActual;
+    }
+
+    public void setCombustible(Combustible combustible) {
+        this.combustible = combustible;
+    }
+    
+    public SistemaKilometraje(Motor motor, Acelerar rpm) {
+        this.kilometrajeTotal = 0.0;
+        this.kilometrajeViaje = 0.0;
+        this.velocidadActual = 0;
+        this.rpm = rpm;
+        this.motor = motor;
+    }
+
+   public void actualizarKilometraje(double distanciaRecorrida) {
+    if (motor.isState()) {
+        kilometrajeTotal += distanciaRecorrida;
+        kilometrajeViaje += distanciaRecorrida;
+
+        if (combustible != null && rpm != null) {
+            combustible.consumir(distanciaRecorrida, rpm.getRpm());
+        }
+    }
+}
+
+   public void resetearKilometrajeViaje() {
+     kilometrajeViaje = 0.0;
+    }
+}
