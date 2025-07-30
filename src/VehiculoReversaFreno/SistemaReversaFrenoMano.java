e2d9f3eac0f99b66df1a114bbaa7f92fe8509b9f
Brandon-Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-07-29 18:08:17 -0600

    Reparacion
    Se reparo la clase de sistema de freno de mano y reversa ya que debia hacer modificaciones, y esta aparecia vacia

--- src/VehiculoReversaFreno/SistemaReversaFrenoMano.java
+++ src/VehiculoReversaFreno/SistemaReversaFrenoMano.java
@@ -9,5 +9,60 @@ package VehiculoReversaFreno;
  * @author Valdelomaar
  */
 public class SistemaReversaFrenoMano {
-    
+
+    private boolean frenoManoActivo;
+    private boolean modoReversa;
+    private boolean sensorReversaActivo;
+
+    public SistemaReversaFrenoMano() {
+        this.frenoManoActivo = false;
+        this.modoReversa = false;
+        this.sensorReversaActivo = false;
+    }
+
+    public void jalarPalancaFrenoMano() {
+        frenoManoActivo = true;
+    }
+
+    public void presionarBotonFrenoMano() {
+        frenoManoActivo = false;
+    }
+
+    public boolean estaFrenoManoActivo() {
+        return frenoManoActivo;
+    }
+
+    public void activarModoReversa() {
+        modoReversa = true;
+        sensorReversaActivo = true;
+    }
+
+    public void desactivarModoReversa() {
+        modoReversa = false;
+        sensorReversaActivo = false;
+    }
+
+    public boolean estaEnReversa() {
+        return modoReversa;
+    }
+
+    public boolean estaSensorReversaActivo() {
+        return sensorReversaActivo;
+    }
+
+    public boolean debeEmitirAlertaPorFrenoYMovimiento() {
+        return frenoManoActivo;
+    }
+
+    public boolean debeEmitirAlertaPorObstaculoEnReversa() {
+        return modoReversa && sensorReversaActivo;
+    }
+
+    public String obtenerEstadoSistema() {
+        StringBuilder estado = new StringBuilder();
+        estado.append("Freno de Mano: ").append(frenoManoActivo ? "Activado" : "Desactivado").append("\n");
+        estado.append("Modo Reversa: ").append(modoReversa ? "Activo" : "Inactivo").append("\n");
+        estado.append("Sensor de Reversa: ").append(sensorReversaActivo ? "Activo" : "Inactivo");
+        return estado.toString();
+    }
 }
