99edc789fdd88e2be7730541c22944bdfc9c21e4
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-07-30 22:32:41 -0600

    Se modifico la clase motor para que solo encienda o apague el vehiculo

--- src/MotorVehiculo/Motor.java
+++ src/MotorVehiculo/Motor.java
@@ -3,51 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MotorVehiculo;
-
-import SistemaRadio.SistemaRadio;
-import AcelerarVehiculo.Acelerar;
-import SistemaClimatizacion.Climatizacion;
 /**
  *
  * @author je110
  */
 public class Motor {
     
-    private boolean state;
-    private SistemaRadio radio;
-    private Acelerar acelerador;
-    private Climatizacion clima;
+private boolean state;
+
+    public Motor() {
+        this.state = false;
+    }
 
     public boolean isState() {
         return state;
     }
 
-    public SistemaRadio getRadio() {
-        return radio;
+    public void encender() {
+        this.state = true;
     }
 
-    public Acelerar getAcelerador() {
-        return acelerador;
-    }
-
-    public Climatizacion getClima() {
-        return clima;
-    }
-    
-    public Motor() {
+    public void apagar() {
         this.state = false;
-        this.radio = new SistemaRadio();
-        this.acelerador = new Acelerar();
-        this.clima = new Climatizacion(this);
-    }
-    
-    public void MotorOn(){
-    this.state = true;
-    radio.encender();
-    }
-    
-    public void MotorOff(){
-    this.state = false;
-    radio.apagar();
     }
 }
