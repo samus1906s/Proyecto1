d4d2555b6a7c0c52131de1db3b5635b0f0e2489b
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-07-30 21:17:37 -0600

    Sistema Kilometraje
    Se modifico la clase Sistema kilometraje por que tenia el error que no package SistemaKilometraje

--- src/SistemaKilometraje/SistemaKilometraje.java
+++ src/SistemaKilometraje/SistemaKilometraje.java
@@ -1,13 +1,14 @@
 
-import AcelerarVehiculo.Acelerar;
-import CombustibleVehiculo.Combustible;
-import MotorVehiculo.Motor;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package SistemaKilometraje;
 
+import AcelerarVehiculo.Acelerar;
+import CombustibleVehiculo.Combustible;
+import MotorVehiculo.Motor;
 /**
  *
  * @author Eduard Salas Murillo
