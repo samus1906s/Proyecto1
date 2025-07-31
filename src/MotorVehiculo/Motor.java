42272dacf5d0ac511b33bc0e9d661ed540fa83c8
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-07-30 20:58:39 -0600

    Clase motor
    Se agregaron sistemas que se encienden automaticamente cuando el vehiculo se arranca

--- src/MotorVehiculo/Motor.java
+++ src/MotorVehiculo/Motor.java
@@ -4,6 +4,8 @@
  */
 package MotorVehiculo;
 
+import SistemaRadio.SistemaRadio;
+import AcelerarVehiculo.Acelerar;
 /**
  *
  * @author je110
@@ -11,20 +13,30 @@ package MotorVehiculo;
 public class Motor {
     
     private boolean state;
+    private SistemaRadio radio;
+    private Acelerar acelerador;
 
     public boolean isState() {
         return state;
     }
 
-    public Motor(boolean state) {
+    public SistemaRadio getRadio() {
+        return radio;
+    }
+    
+    public Motor() {
         this.state = false;
+        this.radio = new SistemaRadio();
+        this.acelerador = new Acelerar();
     }
     
     public void MotorOn(){
     this.state = true;
+    radio.encender();
     }
     
     public void MotorOff(){
     this.state = false;
+    radio.apagar();
     }
 }
